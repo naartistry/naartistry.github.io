2db165df0a537a35e962e837b59d44f1b653aeac
naartistry <naartistryworks@gmail.com>
2026-05-08 11:44:56 +0800

    Repair

---
+++ cms-data.js
@@ -0,0 +1,13 @@
+/**
+ * NA Artistry - Published CMS & Layout Configuration
+ * This file acts as the source of truth for all visitors.
+ * The Admin Dashboard can export data to be pasted here.
+ */
+
+window.NA_PUBLISHED_CONTENT = {
+    // Content overrides go here
+};
+
+window.NA_PUBLISHED_LAYOUT = {
+    // Layout overrides go here
+};
